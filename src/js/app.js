fec79dd05172a73290b98427361d6ef47c9d2d68
Ewa Orzechowska <orzechowska1989@wp.pl>
2021-10-23 22:03:55 +0200

    add changes

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import{settings} from './settings.js'
+const app = {
+    initData: function() {
+        const url = settings.db.url + '/' + settings.db.products;
+        this.data = {};
+        fetch(url)
+            .then((rawResponse) => {
+                return rawResponse.json();
+            })
+            .then((parsedResponse) => {
+                this.data.products = parsedResponse;
+            });
+    },
+    init: function() {
+    },
+}
+
+app.init();
